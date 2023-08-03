7a45af4612cecad3fdbf66269b4e3309c7a5fe2b
Becca B <cerulean.rebecca20@hotmail.com>
2023-08-03 20:54:54 +0100

    added navigation bar foundations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import styles from "./style";
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
-    <div>
-      
+
+    // NAVIGATION BAR
+    <div className={`${styles.paddingX}} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+
+    </div>
+
   </div>
 );
 
