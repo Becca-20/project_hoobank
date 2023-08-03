ff06266b3f520e12f55384e44ea3059c14d6f7d3
Becca B <cerulean.rebecca20@hotmail.com>
2023-08-03 21:20:15 +0100

    added opening & closing tags to structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,28 @@ const App = () => (
     // NAVIGATION BAR
     <div className={`${styles.paddingX}} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     // HERO SECTION
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
 
     // MAIN SECTION
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
 
