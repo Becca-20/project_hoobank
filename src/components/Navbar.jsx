038048fb774182d0a6e3729608103b15f2f703cd
Becca B <cerulean.rebecca20@hotmail.com>
2023-08-03 22:50:30 +0100

    updating mobile view of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='purplebank' className='w-[124px] h-[32px]'/>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
@@ -25,7 +25,31 @@ const Navbar = () => {
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close : menu} />
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+
+          <ul className='list-none flex justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+              >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+        </div>
       </div>
 
     </nav>
