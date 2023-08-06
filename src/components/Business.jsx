6c4f6db0567ba0551d4c3dd37caca0d887900f3d
Becca B <cerulean.rebecca20@hotmail.com>
2023-08-06 16:00:49 +0100

    updated comments

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,10 +3,19 @@ import styles, {layout} from "../style";
 import Button from './Button';
 
 const FeatureCard = ({icon, title, content, index}) => (
+
+  // STYLING FOR CARDS
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+
+    {/* STYLING FOR ICONS */}
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
+
+    <div>
+
+    </div>
+
   </div>
 )
 
