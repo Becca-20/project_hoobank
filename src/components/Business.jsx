366d7237e36b641198d1e26082ced284da9388cb
Becca B <cerulean.rebecca20@hotmail.com>
2023-08-06 16:14:40 +0100

    finished business section & added rounded styling to get started button

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -12,8 +12,14 @@ const FeatureCard = ({icon, title, content, index}) => (
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
 
-    <div>
-
+    {/* STYLING FOR HEADINGS */}
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
     </div>
 
   </div>
