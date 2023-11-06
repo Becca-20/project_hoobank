b8baa70558f941f364d4ab3485693615c8e652da
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2023-11-06 15:25:27 +0000

    finished CardDeal section with image

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -4,7 +4,17 @@ import Button from './Button';
 
 
 const CardDeal = () => (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in a few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor. Aliquet ultrices ac, ametau.</p>
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className="w-[100%] h-[100%]" />
+      </div>
+    </section>
 )
 
 export default CardDeal
