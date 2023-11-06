9460435065ef6b852b90b443abc303977b957435
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2023-11-06 21:13:52 +0000

    finished testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,6 +2,7 @@ import { feedback } from '../constants';
 import styles from '../style';
 import FeedbackCard from './FeedbackCard';
 
+
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
     
