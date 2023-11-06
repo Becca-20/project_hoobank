14d5c54959d19e40647099405f983dd1be645339
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2023-11-06 15:40:33 +0000

    what people are saying about us heading

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,10 +2,17 @@ import { feedback } from '../constants';
 import styles from '../style';
 import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    
+    {/* TO DO: gradient */}
+    <div />
+
+    <div className='w-full flex justify-between items-center md:flex-row flex-col sd:mb-16 mb-16 relative z-[1]'>
+      <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
+    </div>
+
+  </section>
+)
 
 export default Testimonials
