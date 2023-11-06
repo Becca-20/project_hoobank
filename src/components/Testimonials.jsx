2da8ec0e29dd9e2e4b97c845b4bc2607a2e01f02
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2023-11-06 16:38:09 +0000

    added gradient to testimonials - need to add businesses underneath

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,8 +5,7 @@ import FeedbackCard from './FeedbackCard';
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
     
-    {/* TO DO: gradient */}
-    <div />
+    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient' />
 
     <div className='w-full flex justify-between items-center md:flex-row flex-col sd:mb-16 mb-16 relative z-[1]'>
       <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
